a7d16e71493c55adf329fd95cb99aa3dc878bcbb
Lev4and <andrey.levchenko.2001@gmail.com>
2024-02-23 15:50:09 +0500

    Implemented "Hotline" component.

--- src/Microservices/Services/News/NewsAggregator.News.Mvc/wwwroot/js/site.js
+++ src/Microservices/Services/News/NewsAggregator.News.Mvc/wwwroot/js/site.js
@@ -23,6 +23,12 @@ document.addEventListener('DOMContentLoaded', function (e) {
 });
 
 document.addEventListener('DOMContentLoaded', async function (e) {
+    const hotlineHeaderLocalTimeElement = document.querySelector('.hotline-header__local-time strong');
+
+    setInterval(() => {
+        hotlineHeaderLocalTimeElement.innerHTML = new Date().toLocaleTimeString('en-US');
+    }, 250);
+
     const connection = new signalR.HubConnectionBuilder()
         .withUrl('http://localhost:5000/api/notification/news')
         .configureLogging(signalR.LogLevel.Information)
@@ -31,7 +37,25 @@ document.addEventListener('DOMContentLoaded', async function (e) {
     await connection.start();
 
     connection.on('AddedNews', (message) => {
-        console.log(message);
+        const hotlineContainer = document.querySelector('.hotline-container');
+
+        hotlineContainer.insertAdjacentHTML('afterbegin', `
+            <div class="hotline-container__item">
+                <div class="hotline-container__item__added-time">
+                    <strong data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${new Date(message.addedAt).toLocaleTimeString('en-US')}">${new Date(message.addedAt).toLocaleTimeString('en-US')}</strong>
+                </div>
+                <div class="hotline-container__item__content">
+                    <div class="hotline-container__item__content__news-source">
+                        <a href="/sources/${message.editor?.source?.id}" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${message.editor?.source?.title}">
+                            <img class="hotline-container__item__content__news-source__logo" src="${message.editor?.source?.logo?.small}" />
+                        </a>
+                    </div>
+                    <a class="hotline-container__item__content__news-title" href="/news/${message.id}" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${message.title}">
+                        ${message.title}
+                    </a>
+                </div>
+            </div>
+        `);
     });
 });
 
