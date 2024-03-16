ac75a9d0a527fc9ba8b545b69283d9d147ac8288
Lev4and <andrey.levchenko.2001@gmail.com>
2024-03-16 11:05:27 +0500

    Implemented GitHub workflows.

--- shell-scripts/dotnet-devcert/fedora-create-dotnet-devcert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. "$(dirname "$0")/common.sh"
-
-$SUDO cp $CRTFILE "/etc/pki/ca-trust/source/anchors/"
-$SUDO update-ca-trust
-
-cleanup
---
+++ tools/scripts/dotnet/install.sh
@@ -0,0 +1,9 @@
+sudo apt install libnss3-tools
+
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install -y dotnet-host
+sudo apt-get install -y dotnet-sdk-8.0
